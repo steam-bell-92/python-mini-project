69e3f8804b546a7eb9b764ebc9b3df87f77e649f
ashrion <ashrion@users.noreply.github.com>
2026-07-11 18:35:04 +0530

    feat: enhance utils/__init__.py with explicit exports
    - Added exports for new db_manager functions: get_all_scores, clear_scores
    - Added export for print_welcome_banner from banners
    - Added exports for all error_logger functions
    - Added __all__ list for explicit public API
    
    Fixes #1654

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,24 @@
 # Central utilities package
-from utils.db_manager import save_score, get_top_scores
-from utils.banners import print_victory_banner, print_game_over_banner
+from utils.db_manager import (
+    save_score,
+    get_top_scores,
+    get_all_scores,
+    clear_scores,
+)
+from utils.banners import print_victory_banner, print_game_over_banner, print_welcome_banner
+from error_logger import log_exception, summarize_logs, get_recent_errors, clear_logs, safe_run
+
+__all__ = [
+    "save_score",
+    "get_top_scores",
+    "get_all_scores",
+    "clear_scores",
+    "print_victory_banner",
+    "print_game_over_banner",
+    "print_welcome_banner",
+    "log_exception",
+    "summarize_logs",
+    "get_recent_errors",
+    "clear_logs",
+    "safe_run",
+]
